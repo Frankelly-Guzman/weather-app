3413604be8e3eed97144830ae504ce79d59f8f0d
Frankelly Guzman <94994618+UrCodeBuddy@users.noreply.github.com>
2022-11-14 19:52:22 -0500

    initialized site

---
+++ app.js
@@ -0,0 +1,17 @@
+var button = document.querySelector(".button");
+var inputValue = document.querySelector(".inputValue");
+var cityName = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+
+button.addEventListener("click", function () {
+  fetch(
+    "https://api.weatherbit.io/v2.0/current?&key=6ac460af1ac2444784422ca273400e3e&city=" +
+      inputValue +
+      "+&units=I"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
+    .catch((err) => alert("Wrong city name!"));
+});
