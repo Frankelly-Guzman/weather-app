243f20357c8d5d3eb3d85e531aa22e5e51106d4b
Frankelly Guzman <94994618+UrCodeBuddy@users.noreply.github.com>
2023-01-16 22:29:30 -0500

    fixed api request

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-var button = document.querySelector(".button");
-var inputValue = document.querySelector(".inputValue");
-var cityName = document.querySelector(".name");
-var desc = document.querySelector(".desc");
-var temp = document.querySelector(".temp");
+const button = document.querySelector("#submit");
+const inputValue = document.querySelector("#inputValue");
+const cityName = document.querySelector(".name");
+const desc = document.querySelector(".desc");
+const temp = document.querySelector(".temp");
+const sym = document.querySelector(".symbol");
+const icon = document.querySelector("#weather-icon");
 
 button.addEventListener("click", function () {
   fetch(
     "https://api.weatherbit.io/v2.0/current?&key=6ac460af1ac2444784422ca273400e3e&city=" +
-      inputValue +
+      inputValue.value +
       "+&units=I"
   )
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      const dataSource = data.data[0];
+      const nameValue = dataSource["city_name"];
+      const tempValue = dataSource["temp"];
+      const descValue = dataSource["weather"]["description"];
+      const symValue = dataSource["weather"]["icon"];
+
+      cityName.innerHTML = nameValue;
+      temp.innerHTML = tempValue;
+      desc.innerHTML = descValue;
+
+      icon.src =
+        "https://www.weatherbit.io/static/img/icons/" + symValue + ".png";
+    })
     .catch((err) => console.log({ err }));
+  inputValue.value = "";
 });
